095b422c6a9779e3cd0700b0b848461cd2289c1e
StreamsByNight <jayshawn932@gmail.com>
2026-03-29 17:21:43 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './', // Tells Vite index.html is in the main folder
+  root: './', // Confirms index.html is in the main folder
+  base: './', // Helps with path resolution on Render
   build: {
-    outDir: 'dist', // Tells Vite to create the 'dist' folder here
-  }
+    outDir: 'dist',
+    emptyOutDir: true, // Clears the folder before building
+    rollupOptions: {
+      input: './index.html', // Explicitly tells Vite where the entry is
+    },
+  },
 })
