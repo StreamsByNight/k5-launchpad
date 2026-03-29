be53cfb794349420672fd9db5357d05420127d41
StreamsByNight <jayshawn932@gmail.com>
2026-03-29 16:52:21 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: './', // Tells Vite index.html is in the main folder
   build: {
-    outDir: 'dist',
-  },
-  root: './' 
+    outDir: 'dist', // Tells Vite to create the 'dist' folder here
+  }
 })
