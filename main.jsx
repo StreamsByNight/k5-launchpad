b495d539355fe6e0428ef18d27cad5b04cfc6a84
StreamsByNight <jayshawn932@gmail.com>
2026-03-29 16:21:49 -0500

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx' // This points to your App.jsx file
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
